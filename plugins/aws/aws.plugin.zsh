26c9d6a9afe21d16c8ebf502bd5ed6b65bd3ea56
Eugene Brodsky <eugene@teehanlax.com>
2014-09-17 15:15:44 -0400

    exports current AWS profile's key pair as environment variables

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -14,6 +14,7 @@ function agp {
 }
 function asp {
   export AWS_DEFAULT_PROFILE=$1
+  aws_extract_keys
 }
 
 # Makes AWS profile info available to shell prompt. Inspired by git_prompt_info()
@@ -21,6 +22,18 @@ function aws_prompt_info {
 	if [[ -n $(echo $AWS_DEFAULT_PROFILE) ]]; 
 	then
 		echo "$ZSH_THEME_AWS_PROFILE_PREFIX$AWS_DEFAULT_PROFILE$ZSH_THEME_AWS_PROFILE_SUFFIX"
+	fi
+}
+
+# needed by aws-sdk rubygem authentication to aws
+function aws_extract_keys {
+	if [[ -n $(echo $AWS_DEFAULT_PROFILE) ]]; 
+	then
+		profile=`grep -w -A 2 $AWS_DEFAULT_PROFILE $AWS_HOME/config`
+		id=`echo $profile | awk -F "=" '/id/ {print $2}'`
+		secret=`echo $profile | awk -F "=" '/secret/ {print $2}'`
+		export AWS_ACCESS_KEY_ID=$id
+	    export AWS_SECRET_ACCESS_KEY=$secret
 	fi
 }
 
